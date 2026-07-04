fac9c6b3490abb7ee0f977a24842848c66d95c09
Lewis-mbui <lewmugo14479@gmail.com>
2026-07-04 11:39:25 +0300

    write complete terraform configuration

---
+++ week4/tuesday/variables.tf
@@ -0,0 +1,50 @@
+variable "region" {
+  description = "AWS region where the infrastructure will be created."
+  type        = string
+  default     = "af-south-1"
+}
+
+variable "availabiliity_zone" {
+  description = "Availability Zone for the EC2 instance"
+  type        = string
+  default     = "af-south-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for the KianiKiosk API server."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance."
+  type        = string
+  default     = "kijanikiosk-api-staging"
+}
+
+variable "environment" {
+  description = "Environment tag applied to all resources."
+  type        = string
+  default     = "staging"
+}
+
+variable "owner" {
+  description = "Owner tag applied to all resources."
+  type        = string
+  default     = "amina"
+}
+
+variable "key_name" {
+  description = "Existing AWS EC2 key pair used for SSH access"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC where the EC2 instance and security group will be created."
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Public IP address allowed to SSH to the instance in CIDR notation"
+  type        = string
+}
